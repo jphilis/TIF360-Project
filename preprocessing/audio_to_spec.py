3831ebbea4b343f2b744d5d3cdd8856802796c3d
jonatca <jonathan.carlson01@outlook.com>
2024-04-24 14:34:26 +0200

    Refactor audio_to_spec.py for improved performance and file handling

--- preprocessing/audio_to_spec.py
+++ preprocessing/audio_to_spec.py
@@ -25,33 +25,40 @@ def list_files():
             if len(files) > 0:
                 if files[0].endswith(".wav"):
                     nr_files = len(files)
-                    files = np.random.choice(files, int(nr_files / 100), replace=True)
+                    # files = np.random.choice(files, int(nr_files / 100), replace=True)
                     sample_files[folder] = [os.path.join(root, file) for file in files]
     return sample_files
 
 
-def plot_mel_spectrogram_torchaudio(waveform, sample_rate, folder):
+def plot_mel_spectrogram_torchaudio(waveform, sample_rate, folder, filename):
     # waveform, sample_rate = torchaudio.load(wav_file)
-    transform = T.MelSpectrogram(sample_rate=sample_rate, n_mels=128, f_max=200000)
+    transform = T.MelSpectrogram(sample_rate=sample_rate, n_mels=128, f_max=120000)
     mel_spectrogram = transform(waveform)
 
-    plt.figure(figsize=(10, 4))
+    fig = plt.figure(figsize=(10, 4), frameon=True)
+
     plt.imshow(
         mel_spectrogram.log2()[0, :, :].numpy(),
         cmap="hot",
         aspect="auto",
         origin="lower",
     )
-    plt.title("Mel Spectrogram (Torchaudio)")
-    plt.xlabel("Time")
-    plt.ylabel("Frequency")
-    plt.colorbar(format="%+2.0f dB")
+    # ax = plt.Axes(fig, [0.0, 0.0, 1.0, 1.0])
+    # ax.set_axis_off()
+    # fig.add_axes(ax)
+    # plt.title("Mel Spectrogram (Torchaudio)")
+    # plt.xlabel("Time")
+    # plt.ylabel("Frequency")
+    # plt.colorbar(format="%+2.0f dB")
     # plt.show()
     random_nr = np.random.randint(1000)
-    filename = f"mel_spectrograms/{folder}/{random_nr}.png"
+    filename_no_extension = filename.split("//").split(".")[0]
+    filename = f"mel_spectrograms/{folder}/{filename_no_extension}.png"
     if not os.path.exists(f"mel_spectrograms/{folder}"):
         os.makedirs(f"mel_spectrograms/{folder}")
-    plt.savefig(filename)
+    # resample image to 224x224 before saving
+    fig.savefig(filename)
+    print("saved", filename)
 
 
 import torchaudio
@@ -61,52 +68,52 @@ import numpy as np
 import os
 
 
-def plot_mel_spectrogram_torchaudio_new(waveform, sample_rate, folder):
-    # Set the n_fft and hop_length for high-resolution spectrograms
-    n_fft = 2048  # Larger FFT size for better frequency resolution
-    hop_length = 512  # Smaller hop length for finer temporal resolution
-    f_min = 60000  # Minimum frequency in Hz (60 kHz)
-    f_max = 150000  # Maximum frequency in Hz (150 kHz)
+# def plot_mel_spectrogram_torchaudio_new(waveform, sample_rate, folder):
+#     # Set the n_fft and hop_length for high-resolution spectrograms
+#     n_fft = 2048  # Larger FFT size for better frequency resolution
+#     hop_length = 512  # Smaller hop length for finer temporal resolution
+#     f_min = 60000  # Minimum frequency in Hz (60 kHz)
+#     f_max = 150000  # Maximum frequency in Hz (150 kHz)
 
-    # Transform waveform to Mel Spectrogram
-    transform = T.MelSpectrogram(
-        sample_rate=sample_rate,
-        n_fft=n_fft,
-        hop_length=hop_length,
-        n_mels=128,
-        f_min=f_min,
-        f_max=f_max,
-        power=2.0,
-        normalized=False,
-    )
-    mel_spectrogram = transform(waveform)
+#     # Transform waveform to Mel Spectrogram
+#     transform = T.MelSpectrogram(
+#         sample_rate=sample_rate,
+#         n_fft=n_fft,
+#         hop_length=hop_length,
+#         n_mels=128,
+#         f_min=f_min,
+#         f_max=f_max,
+#         power=2.0,
+#         normalized=False,
+#     )
+#     mel_spectrogram = transform(waveform)
 
-    # Convert to decibels
-    mel_spectrogram_db = torchaudio.transforms.AmplitudeToDB()(mel_spectrogram)
+#     # Convert to decibels
+#     mel_spectrogram_db = torchaudio.transforms.AmplitudeToDB()(mel_spectrogram)
 
-    # Plotting
-    plt.figure(figsize=(10, 4))
-    plt.imshow(
-        mel_spectrogram_db.log2()[0, :, :].numpy(),
-        cmap="hot",
-        aspect="auto",
-        origin="lower",
-    )
-    plt.title("Mel Spectrogram (Torchaudio)")
-    plt.xlabel("Time (s)")
-    plt.ylabel("Mel Frequency")
-    plt.colorbar(format="%+2.0f dB")
+#     # Plotting
+#     plt.figure(figsize=(10, 4))
+#     plt.imshow(
+#         mel_spectrogram_db.log2()[0, :, :].numpy(),
+#         cmap="hot",
+#         aspect="auto",
+#         origin="lower",
+#     )
+#     plt.title("Mel Spectrogram (Torchaudio)")
+#     plt.xlabel("Time (s)")
+#     plt.ylabel("Mel Frequency")
+#     plt.colorbar(format="%+2.0f dB")
 
-    # Ensure the directory exists
-    directory_path = f"mel_spectrograms/{folder}"
-    if not os.path.exists(directory_path):
-        os.makedirs(directory_path)
+#     # Ensure the directory exists
+#     directory_path = f"mel_spectrograms/{folder}"
+#     if not os.path.exists(directory_path):
+#         os.makedirs(directory_path)
 
-    # Save the figure
-    random_nr = np.random.randint(1000)
-    filename = f"{directory_path}/{random_nr}.png"
-    plt.savefig(filename)
-    plt.close()
+#     # Save the figure
+#     random_nr = np.random.randint(1000)
+#     filename = f"{directory_path}/{random_nr}.png"
+#     plt.savefig(filename)
+#     plt.close()
 
 
 def resample(signal, sr, target_sample_rate):
@@ -134,20 +141,40 @@ def reshape(signal, target_size):
     return clips
 
 
-target_sr = 350000  # parameter, may be changed
+target_sr = 300000  # parameter, may be changed
 target_size = 2 * target_sr  # parameter, may be changed
 sample_rates = []
 data_lengths = []
 sample_files = list_files()
+img_nr = 0
 for folder, files in sample_files.items():
+    print(folder)
+    print(len(files))
     for file in files:
-        waveform, sample_rate = torchaudio.load(file)
+        if not os.path.exists(f"audio_clips/{folder}"):
+            os.makedirs(f"audio_clips/{folder}")
+        try:
+            waveform, sample_rate = torchaudio.load(file)
+        except Exception as e:
+            print("error loading file", file, e)
+            continue
         if sample_rate != target_sr:
             waveform = resample(waveform, sample_rate, target_sr)
             sample_rate = target_sr
             clips = reshape(waveform, target_size)
+        # save audio clips
         for waveform in clips:
-            plot_mel_spectrogram_torchaudio(waveform, sample_rate, folder)
+            # save audio waveform with same name as before
+            filename = file.split("\\")[-1]
+            print("filename", filename)
+            try:
+                torchaudio.save(
+                    f"audio_clips/{folder}/img{img_nr}.wav", waveform, sample_rate
+                )
+                img_nr += 1
+            except Exception as e:
+                print("error saving file", filename, e)
+            # plot_mel_spectrogram_torchaudio(waveform, sample_rate, folder, file)
         """sample_rates.append(sample_rate)
         num_channels, num_samples = wave_form.shape
         data_lengths.append(num_samples)
