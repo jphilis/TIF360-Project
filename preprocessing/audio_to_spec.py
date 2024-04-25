c24c0b656217387156ca12497c57a96c5edc75ef
Globee <marten.sanderod@gmail.com>
2024-04-25 10:13:58 +0200

    fixed audio to spec file

--- preprocessing/audio_to_spec.py
+++ preprocessing/audio_to_spec.py
@@ -1,3 +1,5 @@
+import argparse
+from pathlib import Path
 import torch
 import torchaudio
 import numpy as np
@@ -11,37 +13,13 @@ print(torch.__version__)
 print(torchaudio.__version__)
 import os
 
-current_dir = os.path.dirname(__file__)
-# parent_dir = os.path.dirname(current_dir)
-main_folder = (
-    r"C:\Users\jonat\OneDrive\chalmers\Advanced neural networks\project\dataset"
-)
-input_data = f"{main_folder}/labeled_dataset"
-output_data = f"{main_folder}/preprocessed_audio"
-if not os.path.exists(output_data):
-    os.makedirs(output_data)
 
-
-def list_files():
-    sample_files = {}
-    for folder in os.listdir(input_data):
-        folder_path = os.path.join(input_data, folder)
-        # take 10 random samples from anywhere in thsi folder, or in a subfolder
-        for root, dirs, files in os.walk(folder_path):
-            if len(files) > 0:
-                if files[0].endswith(".wav"):
-                    nr_files = len(files)
-                    files = np.random.choice(files, 10, replace=True)
-                    sample_files[folder] = [os.path.join(root, file) for file in files]
-    return sample_files
-
-
-def plot_mel_spectrogram_torchaudio(waveform, sample_rate, folder, filename):
+def save_mel_spectrogram(waveform, sample_rate, filename):
     # waveform, sample_rate = torchaudio.load(wav_file)
     transform = T.MelSpectrogram(sample_rate=sample_rate, n_mels=128, f_max=120000)
     mel_spectrogram = transform(waveform)
 
-    fig = plt.figure(figsize=(10, 4), frameon=True)
+    fig = plt.figure(figsize=(10, 4), frameon=False)
 
     plt.imshow(
         mel_spectrogram.log2()[0, :, :].numpy(),
@@ -49,19 +27,18 @@ def plot_mel_spectrogram_torchaudio(waveform, sample_rate, folder, filename):
         aspect="auto",
         origin="lower",
     )
-    # ax = plt.Axes(fig, [0.0, 0.0, 1.0, 1.0])
-    # ax.set_axis_off()
-    # fig.add_axes(ax)
+
+    # plt.plot(mel_spectrogram.log2()[0, :, :].numpy(), cmap="hot", aspect="auto2", origin="lower")
+    ax = plt.Axes(fig, [0.0, 0.0, 1.0, 1.0])
+    ax.set_axis_off()
+    # ax.plot(mel_spectrogram.log2()[0, :, :].numpy(), cmap="hot", aspect="auto", origin="lower")
+    fig.add_axes(ax)
+    plt.axis("off")
     # plt.title("Mel Spectrogram (Torchaudio)")
     # plt.xlabel("Time")
     # plt.ylabel("Frequency")
     # plt.colorbar(format="%+2.0f dB")
     # plt.show()
-    random_nr = np.random.randint(1000)
-    filename_no_extension = filename.split("//").split(".")[0]
-    filename = f"mel_spectrograms/{folder}/{filename_no_extension}.png"
-    if not os.path.exists(f"mel_spectrograms/{folder}"):
-        os.makedirs(f"mel_spectrograms/{folder}")
     # resample image to 224x224 before saving
     fig.savefig(filename)
     print("saved", filename)
@@ -120,46 +97,94 @@ def apply_bandpass_filter(waveform, sample_rate, low_freq, high_freq):
 
 def main():
 
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("input_folder", help="Path to the input folder")
+    parser.add_argument("destination_folder", help="Path to the destination folder")
+    args = parser.parse_args()
+
+    input_folder = Path(args.input_folder)
+    destination_folder = Path(args.destination_folder)
+
+    print(f"\n\nInput folder: {input_folder.absolute()}")
+    print(f"\n\nDestination folder: {destination_folder.absolute()}")
+    i = input("Continue? y/n \n")
+    if i.lower() != "y":
+        print("Exiting...")
+        return
+
     target_sr = 300000  # parameter, may be changed
     target_size = 2 * target_sr  # parameter, may be changed
-    sample_rates = []
-    data_lengths = []
-    sample_files = list_files()
-    img_nr = 0
-    failed_files = 0
-    for folder, files in sample_files.items():
-        print(folder)
-        print(len(files))
-        for file in files:
-            # if not os.path.exists(f"audio_clips/{folder}"):
-            #     os.makedirs(f"audio_clips/{folder}")
-            try:
-                waveform, sample_rate = torchaudio.load(file)
-            except Exception as e:
-                print("error loading file", file, e)
-                continue
-            # if sample_rate != target_sr:
-            waveform = resample(waveform, sample_rate, target_sr)
-            sample_rate = target_sr
-            waveform = apply_bandpass_filter(waveform, sample_rate, 20000, 120000)
-            clips = reshape(waveform, target_size)
-            # save audio clips
-            for waveform in clips:
 
-                # save audio waveform with same name as before
-                filename = file.split("\\")[-1]
-                # print("filename", filename)
-                filepath = f"{output_data}/{folder}/{filename}"
-                if not os.path.exists(f"{output_data}/{folder}"):
-                    os.makedirs(f"{output_data}/{folder}")
-                try:
-                    torchaudio.save(filepath, waveform, sample_rate)
-                    img_nr += 1
-                except Exception as e:
-                    print("error saving file", filename, e)
-                    failed_files += 1
-        print("failed files", failed_files)
-        print("success files", img_nr)
+    #Iterate thorough all folders
+
+    for bat in input_folder.iterdir():
+        print(bat.stem)
+
+        for file in bat.iterdir():
+            print(file.absolute())
+            waveform, sample_rate = torchaudio.load(file.absolute())
+            waveform = resample(waveform, sample_rate, target_sr)
+            waveform = apply_bandpass_filter(waveform, target_sr, 20000, 120000)
+
+            bat_path = Path(destination_folder / bat.stem)
+
+            if not bat_path.exists():
+                bat_path.mkdir(parents=True, exist_ok=True)
+
+            save_destination = Path( destination_folder / bat.stem / file.stem)
+            save_mel_spectrogram(waveform, target_sr, save_destination.absolute())
+
+
+    # sample_rates = []
+    # data_lengths = []
+    # sample_files = list_files()
+    # img_nr = 0
+    # failed_files = 0
+    # for folder, files in sample_files.items():
+    #     print(folder)
+    #     print(len(files))
+    #     for file in files:
+    #         # if not os.path.exists(f"audio_clips/{folder}"):
+    #         #     os.makedirs(f"audio_clips/{folder}")
+    #         try:
+    #             waveform, sample_rate = torchaudio.load(file)
+    #         except Exception as e:
+    #             print("error loading file", file, e)
+    #             continue
+    #         # if sample_rate != target_sr:
+    #         waveform = resample(waveform, sample_rate, target_sr)
+    #         sample_rate = target_sr
+    #         waveform = apply_bandpass_filter(waveform, sample_rate, 20000, 120000)
+    #         clips = reshape(waveform, target_size)
+    #         # save audio clips
+    #         for waveform in clips:
+
+    #             # save audio waveform with same name as before
+    #             filename = file.split("\\")[-1]
+    #             # print("filename", filename)
+    #             filepath = f"{output_data}/{folder}/{filename}"
+    #             if not os.path.exists(f"{output_data}/{folder}"):
+    #                 os.makedirs(f"{output_data}/{folder}")
+    #             try:
+    #                 torchaudio.save(filepath, waveform, sample_rate)
+    #                 img_nr += 1
+    #             except Exception as e:
+    #                 print("error saving file", filename, e)
+    #                 failed_files += 1
+    #     print("failed files", failed_files)
+    #     print("success files", img_nr)
+
+
+
+#Read files
+
+
+
+#Split files
+
+
+#Save files
+
 
 
 if __name__ == "__main__":
