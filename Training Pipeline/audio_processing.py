5188bae7fe591658b5f8ee01f55143e49edaf36f
jonatca <jonathan.carlson01@outlook.com>
2024-05-09 10:53:10 +0200

    detecting noise little better

--- Training Pipeline/audio_processing.py
+++ Training Pipeline/audio_processing.py
@@ -3,6 +3,16 @@ import numpy as np
 import torch
 import torchaudio
 import torchaudio
+import sys
+import os
+from matplotlib import pyplot as plt
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
+batdetect2_main_dir = os.path.join(parent_dir, "batdetect2-main")
+sys.path.append(batdetect2_main_dir)
+print("batdetect2_main_dir", batdetect2_main_dir)
+from batdetect2 import api
 
 
 def exceeds_energy_threshold(waveform, absolute_threshold, max_count):
@@ -68,16 +78,16 @@ def main():
     parser.add_argument("input_folder", help="Path to the input folder")
     parser.add_argument("destination_folder", help="Path to the destination folder")
     args = parser.parse_args()"""
-    data_size_gb = 2
+    # data_size_gb = 2
 
-    target_sr = 300000  # parameter, may be changed
-    target_size = 2 * target_sr  # parameter, may be changed
+    target_sr = 256000  # parameter, may be changed
+    target_size = 25600
     low_freq = 20000
     high_freq = 120000
 
     # for noise/no noise
-    absolute_threshold = 0.0035
-    max_count = 15
+    absolute_threshold = 0.035
+    max_count = 50  # 7500
 
     script_directory = Path(__file__).resolve().parent
 
@@ -86,29 +96,33 @@ def main():
     input_folder = script_directory.parent.parent / "dataset" / "labeled_dataset"
     # destination_folder = script_directory / 'training_data'
     orig_destination_folder = (
-        script_directory.parent.parent / "dataset" / "training_data_2"
+        script_directory.parent.parent
+        / "dataset"
+        / f"training_data_100ms_noise_{max_count}"
     )
 
     # Print the absolute paths of the input folder and destination folder
     print(f"\n\nInput folder: {input_folder.resolve()}")
     print(f"Destination folder: {orig_destination_folder.resolve()}")
-    i = input("Continue? y/n \n")
-    if i.lower() != "y":
-        print("Exiting...")
-        return
+    # i = input("Continue? y/n \n")
+    # if i.lower() != "y":
+    #     print("Exiting...")
+    #     return
 
     # Iterate thorough all folders
     error_files = []
     np.random.seed(42)
 
     for bat in input_folder.iterdir():
+        # if random.random() > 0.01:
+        #     continue
 
         files = list(bat.glob("**/*"))
-        total_size = sum(f.stat().st_size for f in files if f.is_file())
-        if total_size > data_size_gb * 1e9:
-            mean_size = total_size / len(files)
-            sample_size = int(data_size_gb / mean_size)
-            files = np.random.choice(files, sample_size, replace=False)
+        # total_size = sum(f.stat().st_size for f in files if f.is_file())
+        # if total_size > data_size_gb * 1e9:
+        #     mean_size = total_size / len(files)
+        #     sample_size = int(data_size_gb / mean_size)
+        #     files = np.random.choice(files, sample_size, replace=False)
 
         for file in files:
 
@@ -122,12 +136,20 @@ def main():
                 destination_folder = orig_destination_folder / "test"
 
             try:
-                waveform, sample_rate = torchaudio.load(file.absolute())
+                waveform = api.load_audio(file.absolute(), target_samp_rate=256000)
+                waveform = torch.tensor(waveform).unsqueeze(0)
+
+                # print("waveform shape: ", waveform.shape)
+                # print("waveform dtype: ", waveform.dtype)
+                # waveform, sample_rate = torchaudio.load(file.absolute())
+                # print("waveform torch shape: ", waveform.shape)
+                # print("waveform torch dtype: ", waveform.dtype)
+                # sample_rate = 256000
             except Exception as e:
                 print("Error loading file: ", file)
                 error_files.append(file)
                 continue
-            waveform = resample(waveform, sample_rate, target_sr)
+            # waveform = resample(waveform, sample_rate, target_sr)
             waveform_clips = reshape(waveform, target_size)
             for i, clip in enumerate(waveform_clips):
                 clip = apply_bandpass_filter(clip, target_sr, low_freq, high_freq)
@@ -142,6 +164,11 @@ def main():
                 save_destination = Path(bat_path / clip_filename)
                 # try:
                 torchaudio.save(uri=save_destination, src=clip, sample_rate=target_sr)
+                # audio = api.load_audio(save_destination, target_samp_rate=256000)
+                # spec = api.generate_spectrogram(audio)
+                # plt.imshow(spec[0, 0, :, :].cpu().numpy())
+                # plt.savefig(save_destination.with_suffix(".png"))
+
                 # except Exception as e:
                 #     print("Error saving file (part of clip): ", save_destination)
                 #     error_files.append(save_destination)
